04afb0b3cdb444f620a7dbee7b92cb27b4bd0545
onnga-wasabi <yuuto.onnga.23@gmail.com>
2018-06-15 00:36:29 +0900

    完成

--- skullstripping.sh
+++ skullstripping.sh
@@ -8,18 +8,14 @@ do
 	while read src
 	do
 		id=`echo $src | tail -n 1 | awk -F "/" '{print $NF}' | awk -F "_" '{print $NF}' | cut -d '.' -f 1 | sed -e 's/I//'`
-		#echo $id
 		label=`cat ${dname}/*.csv | grep $id | cut -d ',' -f 3 | sed -e 's/\"//g'`
-		#echo $label
 		subject=`cat ${dname}/*.csv | grep $id | cut -d ',' -f 2 | sed -e 's/\"//g'`
-		#echo $subject
 		dst_fname=`echo $src | awk -F '/' '{print $NF}'`
 		dst=`echo ${FREESURFER_DST}/${label}/${subject}/${dst_fname}.gz`
 		if [ ! -e $dst ];then
-			# recon-all -s target -i $src -autorecon1
-			echo $dst
-			# mri_convert $SUBJECTS_DIR/target/mri/brainmask.mgz file.nii.gz
-			echo $SUBJECTS_DIR/target
+			#recon-all -s target -i $src -autorecon1
+			#mri_convert $SUBJECTS_DIR/target/mri/brainmask.mgz $dst
+			#rm -rf $SUBJECTS_DIR/target
 			echo $dst >> ${dname}_done.log
 		else
 			echo -n '[Attention] ' && echo -n ${dst_fname}.gz && echo ' is already exits!'
